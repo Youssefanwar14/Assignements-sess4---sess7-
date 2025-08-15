7b86d4b0dc5df8c8163051931608c728ebf8129a
Youssefanwar14 <youssefmohamed120012@gmail.com>
2025-08-15 10:02:55 +0300

    Add project files.

---
+++ Assignements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
