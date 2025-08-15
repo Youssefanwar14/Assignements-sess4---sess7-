f0fba3567cbb8cbe4ce3345e198c43edbe28d86b
Youssefanwar14 <youssefmohamed120012@gmail.com>
2025-08-15 11:35:20 +0300

    assignement_sess4

---
+++ Assignements/Duration.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignements
+{
+    public class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        // constructor
+        public Duration(int hours,int minutes,int seconds) 
+        {
+            Hours = hours; 
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public Duration(int totalSeconds)
+       
+        {
+            Hours = totalSeconds / 3600;
+            
+            int remainingSeconds = totalSeconds % 3600;
+            
+            Minutes = remainingSeconds / 60;
+
+            Seconds = remainingSeconds % 60;
+        }
+        
+        public int totalSeconds() 
+        {
+            return Hours * 3600 + Minutes * 60 + Seconds;
+        }
+        public override string ToString()
+        {
+            if (Hours != 0)
+                 return $"Hours : {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+            else
+                return $"Minutes: {Minutes}, Seconds: {Seconds}";
+        }
+
+        public static Duration operator +(Duration left, Duration right) 
+        {
+            int totalSeconds = left.totalSeconds() + right.totalSeconds();
+            return new Duration(totalSeconds);
+        
+        }
+        public static Duration operator +(Duration left, int seconds) 
+        {
+            return new Duration(left.totalSeconds() + seconds);
+        
+        }
+        public static Duration operator +(int seconds , Duration right) 
+        {
+            return new Duration( seconds + right.totalSeconds());
+        
+        }
+
+        public static Duration operator ++(Duration d) 
+        {
+            int total = d.totalSeconds();
+            return new Duration(total + 60); 
+        }
+        public static Duration operator --(Duration d) 
+        {
+            int total = d.totalSeconds();
+            return new Duration(total - 60); 
+        }
+
+        public static Duration operator -(Duration left, Duration right) 
+        {
+         int total = left.totalSeconds() - right.totalSeconds();
+         if (total < 0)
+                total = 0;
+         return new Duration(total);
+        }
+
+        public static bool operator >(Duration left, Duration right) 
+        {
+            if(left.totalSeconds() > right.totalSeconds())
+                return true;
+            else 
+                return false;
+            
+        }
+        public static bool operator <(Duration left, Duration right) 
+        {
+            if(left.totalSeconds() < right.totalSeconds())
+                return true;
+            else 
+                return false;
+            
+        }
+
+    }
+}
--- Assignements/Program.cs
+++ Assignements/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Duration D1 = new Duration(1,10,15);
+            Console.WriteLine(D1);
+            
+            Duration D2 = new Duration(3600);
+            Console.WriteLine(D2);
+            
+            Duration D3 = new Duration(7800);
+            Console.WriteLine(D3);
+
+            Duration D4 = new Duration(666);
+            Console.WriteLine(D4);
+
+            D3 = D1 + D2;
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("After adding D3 =>");
+            Console.WriteLine(D3);
+
+            Console.WriteLine("If number right D4 =>");
+            D4 = D1 + 7800;
+            Console.WriteLine(D4);
+
+            Console.WriteLine("If number left D4 =>");
+            D4 = 3600 + D1;
+            Console.WriteLine(D4);
+
+            Console.WriteLine("Increase one minute");
+            D4 = ++D3;
+            Console.WriteLine(D4);
+
+            Console.WriteLine("Decrease one minute");
+            D4 = --D3;
+            Console.WriteLine(D4);
+
+            Console.WriteLine("Subtraction: ");
+            D4 = D1 - D2;
+            Console.WriteLine(D4);
+
+            Console.WriteLine("greater or less than:");
+            Console.WriteLine(D1 > D2);
         }
     }
 }
